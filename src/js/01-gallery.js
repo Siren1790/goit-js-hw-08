a9a4402a42db1fb0290933398e571a9bb4023014
Sergii Karpus <karpus.sergey@gmail.com>
2022-11-08 23:39:05 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const divGalleryRef = document.querySelector(".gallery");
+//Створення розмітки Галереї
+const createGallary = (items) => {
+   return items
+      .map((item) =>
+         `<li><a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+         </a></li>`
+      )
+      .join("");
+};
+console.log(createGallary(galleryItems));
+divGalleryRef.innerHTML = createGallary(galleryItems);
+
+const gallery = new SimpleLightbox('.gallery a', {
+   /* options */
+   captionsData: 'alt',
+   captionDelay: '250'
+});
+// console.log(galleryItems);
