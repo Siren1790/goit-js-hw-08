afacc0a81cc05d9d22885852daa40dfc31acecb0
Sergii Karpus <karpus.sergey@gmail.com>
2022-11-11 01:17:41 +0200

    inProgress

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
-console.log('xxx');
+const feedbackForm = document.querySelector('.feedback-form');
+
+
+const xxx = (x) => {
+   console.log(x);
+   let targetName = x.target.name;
+   let targetValue = x.target.value;
+
+   console.log(targetName);
+   console.log(targetValue);
+}
+
+
+feedbackForm.addEventListener('keyup', xxx);
