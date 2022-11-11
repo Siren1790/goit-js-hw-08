d6c979c6b0d4e869604d9e6625afaaa9217dbf2d
Siren1790 <49605599+Siren1790@users.noreply.github.com>
2022-11-11 18:34:21 +0000

    Deploying to gh-pages from @ Siren1790/goit-js-hw-08@9e012dd5b87736609c51ede3add33f656ac4b0ac 🚀

--- 03-feedback.88cf0561.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("keyup",(function(e){console.log(e);var o=e.target.name,n=e.target.value;console.log(o),console.log(n)}));
-//# sourceMappingURL=03-feedback.88cf0561.js.map
--- 03-feedback.bfc97cc5.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("keyup",(e=>{console.log(e);let o=e.target.name,l=e.target.value;console.log(o),console.log(l)}));
-//# sourceMappingURL=03-feedback.bfc97cc5.js.map
