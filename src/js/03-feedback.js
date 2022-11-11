9e012dd5b87736609c51ede3add33f656ac4b0ac
Sergii Karpus <karpus.sergey@gmail.com>
2022-11-11 20:33:51 +0200

    Update 08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,30 @@
+import throttle from "lodash.throttle";
+
+const keyLocal = 'feedback-form-state';
 const feedbackForm = document.querySelector('.feedback-form');
+const savedSettings = JSON.parse(localStorage.getItem(keyLocal));
 
 
-const xxx = (x) => {
-   console.log(x);
-   let targetName = x.target.name;
-   let targetValue = x.target.value;
 
-   console.log(targetName);
-   console.log(targetValue);
-}
+const objForValues = {
+   ...savedSettings,
+};
 
+const addValuesFromLocStor = function ({ email = '', message = '' }) {
+   feedbackForm.email.value = email;
+   feedbackForm.message.value = message;
+};
 
-feedbackForm.addEventListener('keyup', xxx);
+if (savedSettings)
+   addValuesFromLocStor(objForValues);
+
+const saveLocalStorage = (evt) => {
+   objForValues[evt.target.name] = evt.target.value;
+   localStorage.setItem(keyLocal, JSON.stringify(objForValues));
+};
+
+// feedbackForm.submit.onclick = (event) => {
+//    event.preventDefault()
+//    console.log('xxx');
+// }
+feedbackForm.addEventListener('input', throttle(saveLocalStorage, 2000));
